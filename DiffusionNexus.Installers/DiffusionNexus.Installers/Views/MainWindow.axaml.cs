1646f67dde4372d99b3cf2409a2fb90212978b7a
Little-God1983 <33689122+Little-God1983@users.noreply.github.com>
2025-10-24 09:07:26 +0200

    Refactor MainWindow by removing unused services
    Removed unused fields and methods related to Git repository interactions and storage services.

--- DiffusionNexus.Installers/DiffusionNexus.Installers/Views/MainWindow.axaml.cs
+++ DiffusionNexus.Installers/DiffusionNexus.Installers/Views/MainWindow.axaml.cs
@@ -11,143 +11,17 @@ namespace DiffusionNexus.Installers.Views
 {
     public partial class MainWindow : Window
     {
-        private readonly DataGrid? _gitRepositoriesGrid;
-        private MainWindowViewModel? _attachedViewModel;
-
         public MainWindow()
         {
             InitializeComponent();
-            _gitRepositoriesGrid = this.FindControl<DataGrid>("GitRepositoriesGrid");
             DataContextChanged += OnDataContextChanged;
         }
 
         private void OnDataContextChanged(object? sender, EventArgs e)
         {
-            if (_attachedViewModel is not null)
-            {
-                _attachedViewModel.EditRepositoryRequested -= OnEditRepositoryRequested;
-            }
-
             if (DataContext is MainWindowViewModel vm)
             {
-                vm.AttachStorageInteraction(new AvaloniaStorageInteractionService(this));
                 vm.AttachGitRepositoryInteraction(new AvaloniaGitRepositoryInteractionService(this));
-                vm.EditRepositoryRequested += OnEditRepositoryRequested;
-                _attachedViewModel = vm;
-            }
-            else
-            {
-                _attachedViewModel = null;
-            }
-        }
-
-        private void OnEditRepositoryRequested(object? sender, GitRepositoryItemViewModel repository)
-        {
-            if (_gitRepositoriesGrid is null)
-            {
-                return;
-            }
-
-            _gitRepositoriesGrid.SelectedItem = repository;
-            _gitRepositoriesGrid.Focus();
-        }
-
-        private sealed class AvaloniaStorageInteractionService : IStorageInteractionService
-        {
-            private static readonly FilePickerFileType AllFilesFilter = new("All files")
-            {
-                Patterns = new[] { "*" }
-            };
-
-            private readonly Window _window;
-
-            public AvaloniaStorageInteractionService(Window window)
-            {
-                _window = window;
-            }
-
-            public async Task<string?> PickOpenFileAsync(CancellationToken cancellationToken = default)
-            {
-                if (!_window.StorageProvider.CanOpen)
-                {
-                    return null;
-                }
-
-                var options = new FilePickerOpenOptions
-                {
-                    AllowMultiple = false,
-                    FileTypeFilter = new[]
-                    {
-                        new FilePickerFileType("Configuration files")
-                        {
-                            Patterns = new[] { "*.json", "*.xml" }
-                        }
-                    }
-                };
-
-                var results = await _window.StorageProvider.OpenFilePickerAsync(options);
-                return results?.FirstOrDefault()?.TryGetLocalPath();
-            }
-
-            public async Task<string?> PickSaveFileAsync(string? suggestedFileName, CancellationToken cancellationToken = default)
-            {
-                if (!_window.StorageProvider.CanSave)
-                {
-                    return null;
-                }
-
-                var options = new FilePickerSaveOptions
-                {
-                    SuggestedFileName = string.IsNullOrWhiteSpace(suggestedFileName)
-                        ? "configuration.json"
-                        : System.IO.Path.GetFileName(suggestedFileName),
-                    FileTypeChoices = new[]
-                    {
-                        new FilePickerFileType("JSON configuration")
-                        {
-                            Patterns = new[] { "*.json" }
-                        },
-                        new FilePickerFileType("XML configuration")
-                        {
-                            Patterns = new[] { "*.xml" }
-                        }
-                    }
-                };
-
-                var result = await _window.StorageProvider.SaveFilePickerAsync(options);
-                return result?.TryGetLocalPath();
-            }
-
-            public async Task<string?> PickFolderAsync(CancellationToken cancellationToken = default)
-            {
-                if (!_window.StorageProvider.CanOpen)
-                {
-                    return null;
-                }
-
-                var results = await _window.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
-                {
-                    AllowMultiple = false
-                });
-
-                return results?.FirstOrDefault()?.TryGetLocalPath();
-            }
-
-            public async Task<string?> PickInterpreterAsync(CancellationToken cancellationToken = default)
-            {
-                if (!_window.StorageProvider.CanOpen)
-                {
-                    return null;
-                }
-
-                var options = new FilePickerOpenOptions
-                {
-                    AllowMultiple = false,
-                    FileTypeFilter = new[] { AllFilesFilter }
-                };
-
-                var results = await _window.StorageProvider.OpenFilePickerAsync(options);
-                return results?.FirstOrDefault()?.TryGetLocalPath();
             }
         }
 
